154643d6b48dd8a3c7d18f694ea8c7a39169bfc5
Mikoding <mikaioharison038@gmail.com>
2023-02-23 13:56:25 -0300

    Updating files

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ContatoController;
+
+Route::get('/contatos',[App\Http\Controllers\ContatoController::class,'index']);
+Route::post('/save',[App\Http\Controllers\ContatoController::class,'store']);
+Route::put('/update/{id}',[App\Http\Controllers\ContatoController::class,'update']);
+Route::delete('/delete/{id}',[App\Http\Controllers\ContatoController::class,'destroy']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
